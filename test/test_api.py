3b415c34712e16a55a6cd35c840eac5a9f503d43
BOL P51 <p51@v4v.app>
2023-05-30 09:11:45 +0300

    Extend tests
    Signed-off-by: BOL P51 <p51@v4v.app>

--- test/test_api.py
+++ test/test_api.py
@@ -1,3 +1,4 @@
+import json
 from unittest.mock import patch
 
 from fastapi.testclient import TestClient
@@ -9,19 +10,24 @@ from guid_slurp.startup import MONGODB_COLLECTION, MONGODB_DATABASE
 client = TestClient(app)
 
 
-def test_resolve_guid():
-    # Create a mock MongoClient
+def setup_mongo_mock():
+    """
+    Setup the mongo mocks
+    """
     mock_mongo_client = MongoClient()
     mock_db = mock_mongo_client[MONGODB_DATABASE]
     mock_collection = mock_db[MONGODB_COLLECTION]
+    with open("test/test_data/podcastGuidUrl.guidUrl.json") as f:
+        mock_collection_data = json.load(f)
 
-    one_record = {
-        "url": "https://example.com/feed.rss",
-        "podcastGuid": "02e12f05-9dc2-4b28-b604-662d7507d435",
-    }
+    mock_collection.insert_many(mock_collection_data)
 
-    # Add a mock result
-    mock_collection.insert_one(one_record)
+    return mock_mongo_client, mock_collection
+
+
+def test_resolve_guid():
+    # Create a mock MongoClient
+    mock_mongo_client, mock_collection = setup_mongo_mock()
 
     # Patch MongoClient
     with patch(
@@ -29,37 +35,27 @@ def test_resolve_guid():
     ) as mock:  # Replace 'your_app' with the module where MongoClient is imported
         mock.return_value = mock_mongo_client
         # Valid URL test
-        response = client.get("/guid/02e12f05-9dc2-4b28-b604-662d7507d435")
+        response = client.get("/guid/856cd618-7f34-57ea-9b84-3600f1f65e7f")
         assert response.status_code == 200
         json_resp = response.json()
-        assert json_resp[0].get("url") == "https://example.com/feed.rss"
-        assert json_resp[0].get("podcastGuid") == "02e12f05-9dc2-4b28-b604-662d7507d435"
+        assert json_resp[0].get("url") == "http://feed.nashownotes.com/rss.xml"
+        assert json_resp[0].get("podcastGuid") == "856cd618-7f34-57ea-9b84-3600f1f65e7f"
 
         # Invalid URL test
         response = client.get("/guid/not-a-valid-uuid")
-        assert response.status_code == 400
-        assert response.json() == {"detail": "Bad GUID"}
+        assert response.status_code == 422
+        assert response.json()["detail"][0]["type"] == "type_error.uuid"
 
         # Not found test
         mock_collection.delete_many({})  # No results found
-        response = client.get("/guid/02e12f05-9dc2-4b28-b604-662d7507d436")
+        response = client.get("/guid/1a4e9748-4296-5a32-b017-bd36bb47e17d")
         assert response.status_code == 404
         assert response.json() == {"detail": "Item not found"}
 
 
 def test_resolve_url():
     # Create a mock MongoClient
-    mock_mongo_client = MongoClient()
-    mock_db = mock_mongo_client[MONGODB_DATABASE]
-    mock_collection = mock_db[MONGODB_COLLECTION]
-
-    one_record = {
-        "url": "https://example.com/feed.rss",
-        "podcastGuid": "02e12f05-9dc2-4b28-b604-662d7507d435",
-    }
-
-    # Add a mock result
-    mock_collection.insert_one(one_record)
+    mock_mongo_client, mock_collection = setup_mongo_mock()
 
     # Patch MongoClient
     with patch(
@@ -67,19 +63,49 @@ def test_resolve_url():
     ) as mock:  # Replace 'your_app' with the module where MongoClient is imported
         mock.return_value = mock_mongo_client
         # Valid URL test
-        response = client.get("/url/?url=https://example.com/feed.rss")
+        response = client.get("/url/?url=http://feed.nashownotes.com/rss.xml")
         assert response.status_code == 200
         json_resp = response.json()
-        assert json_resp[0].get("url") == "https://example.com/feed.rss"
-        assert json_resp[0].get("podcastGuid") == "02e12f05-9dc2-4b28-b604-662d7507d435"
+        assert json_resp[0].get("url") == "http://feed.nashownotes.com/rss.xml"
+        assert json_resp[0].get("podcastGuid") == "856cd618-7f34-57ea-9b84-3600f1f65e7f"
 
         # Invalid URL test
         response = client.get("/url/?url=not-a-valid-url")
-        assert response.status_code == 400
-        assert response.json() == {"detail": "Bad RSS URL"}
+        assert response.status_code == 422
+        assert response.json()["detail"][0]["type"] == "value_error.url.scheme"
+
 
         # Not found test
         mock_collection.delete_many({})  # No results found
         response = client.get("/url/?url=https://example.com/not_found.rss")
+        assert response.status_code == 404
+        assert response.json() == {"detail": "Item not found"}
+
+
+def test_resolve_itunesId():
+    # Create a mock MongoClient
+    mock_mongo_client, mock_collection = setup_mongo_mock()
+
+    # Patch MongoClient
+    with patch(
+        "guid_slurp.main.MongoClient"
+    ) as mock:  # Replace 'your_app' with the module where MongoClient is imported
+        mock.return_value = mock_mongo_client
+        # Valid URL test
+        response = client.get("/itunesId/269169796")
+        assert response.status_code == 200
+        json_resp = response.json()
+        assert json_resp[0].get("url") == "http://feed.nashownotes.com/rss.xml"
+        assert json_resp[0].get("podcastGuid") == "856cd618-7f34-57ea-9b84-3600f1f65e7f"
+
+        # Invalid URL test
+        response = client.get("/itunesId/not-a-valid-id")
+        assert response.status_code == 422
+        assert response.json()["detail"][0]["type"] == "type_error.integer"
+
+
+        # Not found test
+        mock_collection.delete_many({})  # No results found
+        response = client.get("/itunesId/9999999")
         assert response.status_code == 404
         assert response.json() == {"detail": "Item not found"}
